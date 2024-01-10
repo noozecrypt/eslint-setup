4c19873b67a03b0531d66c6693c7a4eeb18a2f07
noozecrypt <noozecrypt@criptext.com>
2024-01-10 23:30:09 +0530

    modified .eslintrc.json file - added a custom eslint rule added two new .js files added .eslintignore file

---
+++ js/getPayments.js
@@ -0,0 +1,10 @@
+// function getPayments() {
+// 	return 1;
+// }
+
+function getLatestPayments() {
+	return 'latest payments';
+}
+
+getLatestPayments();
+// getPayments(); //  getPayments is deprecated. Use getLatestPayments instead  find-payments - find-payments custom rule - eslint error
